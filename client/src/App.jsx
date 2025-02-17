673b4645d03347c8826ba4a1b1873571f80a4446
Beprodeep Das <codingbtech@gmail.com>
2025-02-17 16:18:15 +0530

    responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import PharmacyStore from './components/MedicalStore/PharmacyStore';
-import MedicineAlarm from './components/MedicineAlarm';
-import Advantages from './components/Advantages';
-import Signin from './components/Pages/Signin';
-import Signup from './components/Pages/Signup';
-import Profile from './components/Pages/Profile';
-import Footer  from './components/Footer';
-import ProductCard from './components/MedicalStore/ProductCard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import PharmacyStore from "./components/MedicalStore/PharmacyStore";
+import MedicineAlarm from "./components/MedicineAlarm";
+import Advantages from "./components/Advantages";
+import Signin from "./components/Pages/Signin";
+import Signup from "./components/Pages/Signup";
+import Profile from "./components/Pages/Profile";
+import Footer from "./components/Footer";
+import ProductCard from "./components/MedicalStore/ProductCard";
 
 const App = () => {
   return (
     <Router>
-      
       <Routes>
-       
-        <Route path="/" element={
-          <>
-            <Navbar /> 
-            <Hero />
-            <Services />
-            <Advantages/>
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Hero />
+              <Services />
+              <Advantages />
+            </>
+          }
+        />
 
-        <Route path="/sign-in" element={<Signin/>} />
-        <Route path="/sign-up" element={<Signup/>} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/pharmacy-store" element={<PharmacyStore />} />
         <Route path="/medicine-alarm" element={<MedicineAlarm />} />
         <Route path="/product/:id" element={<ProductCard />} />
-
-
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 };
